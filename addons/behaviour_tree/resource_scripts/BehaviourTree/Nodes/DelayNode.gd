b7a1446ade3c1be8bee0ace2319d6f8d5db2b1a7
Swirtez <themasterprogrammer70000@gmail.com>
2026-03-24 21:01:40 +0530

    a small change in delay decorator

--- addons/behaviour_tree/resource_scripts/BehaviourTree/Nodes/DelayNode.gd
+++ addons/behaviour_tree/resource_scripts/BehaviourTree/Nodes/DelayNode.gd
@@ -7,9 +7,9 @@ var _start_time: int = -1
 
 func _tick() -> NodeStatus.status:
 	if _start_time == -1:
-		_start_time = Time.get_ticks_msec()
+		_start_time = Time.get_unix_time_from_system()
 	
-	var elapsed :float = (Time.get_ticks_msec() - _start_time) / 1000.0
+	var elapsed :float = (Time.get_unix_time_from_system() - _start_time)
 	if elapsed < delay_seconds:
 		return NodeStatus.status.RUNNING
 	
