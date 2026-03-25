8c6be2f90168555bbdea9fa929ce51e1d7a8814a
Swirtez <themasterprogrammer70000@gmail.com>
2026-03-25 08:52:51 +0530

    add oneshot to delay

--- addons/behaviour_tree/resource_scripts/BehaviourTree/Nodes/DelayNode.gd
+++ addons/behaviour_tree/resource_scripts/BehaviourTree/Nodes/DelayNode.gd
@@ -3,17 +3,24 @@ class_name DelayNode
 
 @export var delay_seconds: float = 1.0
 
+@export var oneshot:bool = false
 var _start_time: int = -1
+var _delay_complete:bool = false
 
 func _tick() -> NodeStatus.status:
-	if _start_time == -1:
-		_start_time = Time.get_unix_time_from_system()
+	if not _delay_complete:
+		if _start_time == -1:
+			_start_time = Time.get_unix_time_from_system()
 	
-	var elapsed :float = (Time.get_unix_time_from_system() - _start_time)
-	if elapsed < delay_seconds:
-		return NodeStatus.status.RUNNING
-	
+		var elapsed :float = (Time.get_unix_time_from_system() - _start_time)
+		if elapsed < delay_seconds:
+			return NodeStatus.status.RUNNING
+		
+		if oneshot:
+			_delay_complete = true
+		
 	var result :NodeStatus.status = decorating_node._tick()
 	if result != NodeStatus.status.RUNNING:
-		_start_time = -1
+		if not oneshot:
+			_start_time = -1
 	return result
--- src/write.gd
+++ src/write.gd
@@ -1,6 +1,12 @@
 extends ActionNode
 
+var times:int  = 0
+
 func _tick()->NodeStatus.status:
+	if times <= 10:
+		times += 1
+	if times > 10:
+		return NodeStatus.status.FALIURE
 	print("writing health")
 	black_board._set_value("health",black_board._get_value("health",0)+10)
 	return NodeStatus.status.SUCCESS
